bced0897ce347fc714ae47c6ae232edd09cd15f8
dylann930 <113508130+dylann930@users.noreply.github.com>
2022-11-01 20:03:43 +0100

    Add files via upload

---
+++ M03_PROGRAMACIO BASICA/Activitat2/Video y ejercicios/DEBUG/src/cat/institutmvm/DEBUG.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package cat.institutmvm;
+
+/*
+ * Nom: Dylan
+ * Cognoms: Osorio Camuña
+ * INS Manuel Vázquez Montalbán
+ * Data d'edició: 27/10/2022
+ * Nom del cicle formatiu: Administració de sistemes informàtics en xarxa.
+ * Nom del mòdul: M03 - Programació básica.
+ */
+public class DEBUG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // EJERCICIO 7 (APARTADO C) ANDREU.
+        int d = 0;
+        while (d<=200) {
+                System.out.println(d);
+            d+=2; 
+        }
+    }
+}      
